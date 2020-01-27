8ec7f3975ec2193b621bc15d3f0fa1321827186a
Thibault Friedrich <thibault.friedrich@gmail.com>
2020-01-27 07:34:22 -0500

    feat: change contact background

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Particles from 'react-particles-js'
+
 import Layout from '~organisms/layout'
 import SEO from '~atoms/seo'
 import Section from '~organisms/section'
@@ -9,10 +11,18 @@ import emailIcon from '~assets/email'
 
 import style from './contact.style'
 
+import particles from './particles' // https://codepen.io/pen/?&editable=true
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Contact" />
     <Section className={style.Contact} center title="Contact me">
+      <Particles
+        className={style.background}
+        width="100%"
+        height="100%"
+        params={particles}
+      />
       <p>Some needs ? Contact me using one of these media:</p>
       <div className={style.socials}>
         <Social
