79c81a89db53e3aba2a6017f1289c06242e1742c
Thibault Friedrich <thibault.friedrich@gmail.com>
2022-01-11 18:31:06 -0500

    fix: add support for large image for twitter

--- src/atoms/seo.jsx
+++ src/atoms/seo.jsx
@@ -92,7 +92,7 @@ function SEO({ description, lang, title, image }) {
         },
         {
           name: 'twitter:card',
-          content: 'summary',
+          content: 'summary_large_image',
         },
         {
           name: 'twitter:creator',
