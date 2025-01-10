849060bd2767e6dff039eae76199c25be876ff08
Thibault Friedrich <thibault.friedrich@gmail.com>
2025-01-10 07:40:47 -0500

    refactor: add prettier config

--- src/app/components/Socials/Socials.tsx
+++ src/app/components/Socials/Socials.tsx
@@ -2,27 +2,25 @@ import { cn } from '@/lib/utils'
 import { socialsList } from './socialsList'
 
 export interface SocialsProps {
-    className?: string
+  className?: string
 }
 
 export function Socials({ className }: SocialsProps) {
-    return (
-        <div
-            className={cn('flex items-center justify-center gap-8', className)}
+  return (
+    <div className={cn('flex items-center justify-center gap-8', className)}>
+      {socialsList.map((social) => (
+        <a
+          key={social.name}
+          href={social.url}
+          target="_blank"
+          className="flex p-1 items-center text-foreground/70 hover:text-foreground transition-colors"
         >
-            {socialsList.map((social) => (
-                <a
-                    key={social.name}
-                    href={social.url}
-                    target="_blank"
-                    className="flex p-1 items-center text-foreground/70 hover:text-foreground transition-colors"
-                >
-                    <span className="sr-only">{social.name}</span>
-                    <div className="w-7 3xl:w-9 aspect-square flex items-stretch justify-center">
-                        {social.icon}
-                    </div>
-                </a>
-            ))}
-        </div>
-    )
+          <span className="sr-only">{social.name}</span>
+          <div className="w-7 3xl:w-9 aspect-square flex items-stretch justify-center">
+            {social.icon}
+          </div>
+        </a>
+      ))}
+    </div>
+  )
 }
