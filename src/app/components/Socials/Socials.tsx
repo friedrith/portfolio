0ecb147c0eb08ebb585ff54e1ac6da29bd901ee5
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-07-24 06:11:49 -0400

    fix: socials in mid size screens

--- src/app/components/Socials/Socials.tsx
+++ src/app/components/Socials/Socials.tsx
@@ -8,12 +8,15 @@ export interface SocialsProps {
 export function Socials({ className }: SocialsProps) {
   return (
     <div className={cn('flex items-center justify-center gap-8', className)}>
-      {socialsList.map((social) => (
+      {socialsList.map((social, index) => (
         <a
           key={social.name}
           href={social.url}
           target="_blank"
-          className="flex p-1 items-center text-foreground/70 hover:text-foreground transition-colors"
+          className={cn(
+            'flex p-1 items-center text-foreground/70 hover:text-foreground transition-colors',
+            index >= 4 ? 'hidden 2xl:flex' : ''
+          )}
         >
           <span className="sr-only">{social.name}</span>
           <div className="w-7 3xl:w-9 aspect-square flex items-stretch justify-center">
