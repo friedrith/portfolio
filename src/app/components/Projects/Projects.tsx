c1330c3ea3667e2cabdaff7dc3cc453f1e8e8140
Thibault Friedrich <thibault.friedrich@interaction-dynamics.io>
2025-12-15 06:10:08 -0500

    feat: add features-cli project

--- src/app/components/Projects/Projects.tsx
+++ src/app/components/Projects/Projects.tsx
@@ -1,34 +1,34 @@
-import Image from 'next/image'
-import { projectsList } from './projectsList'
+import Image from "next/image";
+import { projectsList } from "./projectsList";
 
 export function Projects() {
-  return (
-    <div className="flex flex-col gap-2 overflow-auto">
-      {projectsList.map((project) => (
-        <a
-          className="flex flex-row items-start gap-5 font-sans bg-transparent hover:bg-muted p-4 rounded-lg transition-colors duration-500"
-          key={project.name}
-          href={project.url}
-          target="_blank"
-        >
-          <div className="relative w-14 aspect-square rounded-md overflow-hidden bg-white">
-            <Image
-              src={project.image}
-              alt={project.name}
-              fill
-              style={{ objectFit: 'cover' }}
-            />
-          </div>
-          <div className="flex-1">
-            <h4 className="text-lg font-bold text-foreground">
-              {project.name}
-            </h4>
-            <p className="mt-1 text-sm font-normal text-muted-foreground">
-              {project.description}
-            </p>
-          </div>
-        </a>
-      ))}
-    </div>
-  )
+	return (
+		<div className="flex flex-col gap-2 overflow-auto">
+			{projectsList.map((project) => (
+				<a
+					className="flex flex-row items-start gap-5 font-sans bg-transparent hover:bg-muted p-4 rounded-lg transition-colors duration-500"
+					key={project.name}
+					href={project.url}
+					target="_blank"
+				>
+					<div className="relative w-14 aspect-square rounded-md overflow-hidden bg-white">
+						<Image
+							src={project.image}
+							alt={project.name}
+							fill
+							style={{ objectFit: "cover" }}
+						/>
+					</div>
+					<div className="flex-1">
+						<h4 className="text-lg font-bold text-foreground">
+							{project.name}
+						</h4>
+						<p className="mt-1 text-sm font-normal text-muted-foreground">
+							{project.description}
+						</p>
+					</div>
+				</a>
+			))}
+		</div>
+	);
 }
--- src/app/components/Projects/projectsList.tsx
+++ src/app/components/Projects/projectsList.tsx
@@ -1,42 +1,48 @@
 export const projectsList = [
-  {
-    name: 'Ship-it',
-    description: 'A next.js boilerplate to kickstart your project faster',
-    url: 'https://github.com/interaction-dynamics/ship-it',
-    image: '/node-wifi.webp',
-  },
-  {
-    name: 'Design System Hub',
-    description: 'An alternative to Storybook',
-    url: 'https://design-system-hub.com/',
-    image: '/design-system-hub.png',
-  },
-  {
-    name: 'Contributing generator',
-    description: 'A CLI to generate markdown files for projects',
-    url: 'https://github.com/friedrith/contributing-generator',
-    image: '/contributing-generator.png',
-  },
-  {
-    name: 'Learn_frontend',
-    description: 'An opinionated guide to become a better frontend developer',
-    url: 'https://learn.interaction-dynamics.io/',
-    image: '/learn_frontend.webp',
-  },
-  {
-    name: 'Node-wifi',
-    description: 'A node library to manage wifi connections',
-    url: 'https://github.com/friedrith/node-wifi',
-    image: '/node-wifi.webp',
-  },
-  // {
-  //   name: 'Password vault',
-  //   description: 'A design system for the web',
-  //   url: '',
-  // },
-  // {
-  //   name: 'Password generator',
-  //   description: 'A design system for the web',
-  //   url: '',
-  // },
-]
+	{
+		name: "Features-cli",
+		description: "A CLI and Web tool to explore the features in your codebase",
+		url: "https://github.com/interaction-dynamics/features",
+		image: "/feature-icon.svg",
+	},
+	{
+		name: "Ship-it",
+		description: "A next.js boilerplate to kickstart your project faster",
+		url: "https://github.com/interaction-dynamics/ship-it",
+		image: "/node-wifi.webp",
+	},
+	{
+		name: "Design System Hub",
+		description: "An alternative to Storybook",
+		url: "https://design-system-hub.com/",
+		image: "/design-system-hub.png",
+	},
+	{
+		name: "Contributing generator",
+		description: "A CLI to generate markdown files for projects",
+		url: "https://github.com/friedrith/contributing-generator",
+		image: "/contributing-generator.png",
+	},
+	{
+		name: "Learn_frontend",
+		description: "An opinionated guide to become a better frontend developer",
+		url: "https://learn.interaction-dynamics.io/",
+		image: "/learn_frontend.webp",
+	},
+	{
+		name: "Node-wifi",
+		description: "A node library to manage wifi connections",
+		url: "https://github.com/friedrith/node-wifi",
+		image: "/node-wifi.webp",
+	},
+	// {
+	//   name: 'Password vault',
+	//   description: 'A design system for the web',
+	//   url: '',
+	// },
+	// {
+	//   name: 'Password generator',
+	//   description: 'A design system for the web',
+	//   url: '',
+	// },
+];
